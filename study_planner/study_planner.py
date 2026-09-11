0f0172d68d889084041fd64c98779e59f38d6ed8
seagulls-9 <freddiethompson2110888@gmail.com>
2026-09-11 18:24:24 +0100

    Enhance study planner with task completion feature
    Added functionality to complete tasks and improved task viewing.

--- study_planner/study_planner.py
+++ study_planner/study_planner.py
@@ -3,14 +3,22 @@ def add_task(tasks)
     task = input("Enter a task: ")
         tasks.append(task)
 def view_tasks(tasks):
-    for task in tasks:
-    print(task)
-    
+    for i in range(len(tasks)):
+        print(f"{i +1}. {tasks[i]}")
+def complete_tasks(tasks):
+    comptask = int(input("What is the number of the completed task? ")) - 1
+
+    if 0 <= comptask < len(tasks):
+        tasks.pop(comptask)
+        print("Task removed. Well done!")
+    else:
+        print("Invalid task number.")
 while True:
     print("--- Study Planner ---")
     print("1. Add Task")
     print("2. View Tasks")
-    print("3. Exit")
+    print("3. Complete tasks")
+    print("4. Exit")
 
     choice = input("Choose an option: ")
 
@@ -19,8 +27,11 @@ while True:
 
     elif choice == "2":
         view_tasks(tasks)
+    
+    elif choice == "3"
+        complete_tasks(tasks)
 
-    elif choice == "3":
+    elif choice == "4":
         print("Goodbye")
         break
 
